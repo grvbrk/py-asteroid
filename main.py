ffd22d68dd791d685e80e0567476a7447144d9da
grvbrk <gouravbarikofficial@gmail.com>
2024-10-03 23:40:41 +0530

    player rotation and movement

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
-  # print("Starting asteroids!")
-  # print(f"Screen width: {SCREEN_WIDTH}")
-  # print(f"Screen height: {SCREEN_HEIGHT}")
 
-  pygame.init()
-  screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-  while True:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        return
-    screen.fill("black")
-    pygame.display.flip()
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        dt = clock.tick(float(60)) / 1000
 
 
 if __name__ == "__main__":
-  main()
+    main()
